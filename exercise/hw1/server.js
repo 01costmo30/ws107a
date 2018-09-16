3f42323a82d2198bafcb1a332b85d2cfd22ad2e9
01costmo30 <01lu301998@gmail.com>
2018-09-16 17:55:45 +0800

    add hw file

--- exercise/hw1/server.js
+++ exercise/hw1/server.js
@@ -18,7 +18,7 @@ const server = http.createServer((req, res) => {
             res.end("<html><head><meta charset='utf-8'><title>My id</title><style>body {background-color: #30FFBA; font-family: consolas;}.subcontent{transform: translate(-50%,-50%);position: absolute;display: table-cell;padding: 40px 40px;width: 30%;top: 50%;left: 50%;}.subcontent span{float: left; width: 100%;}.clear {clear: both;}</style></head><body><div class='subcontent'><span style = 'font-size: 6vw; font-weight: bold;'>我的學號:</span><span style='font-size: 6vw;'>110510501</span><div class='clear'></div></div></body></html>");
             break;
         default:
-            res.writeHead(200, {"Content-Type": "text/html;charset='utf-8'"});
+            res.writeHead(404, {"Content-Type": "text/html;charset='utf-8'"});
             res.end("<html><head><meta charset='utf-8'><title>404</title><style>body {background-color: #30E6FF;}.subcontent{transform: translate(-50%,-50%);position: absolute;display: table-cell;padding: 50px 40px;width: 30%;top: 50%;left: 50%;}.subcontent span{float: left; width: 100%;}.clear {clear: both;}</style></head><body><div class='subcontent'><span style = 'font-size: 10vw;'>404: </span><span style = 'font-size: 3vw;'>Oops! Wrong page.</span><div class='clear'></div></div></body></html>");
             break;
     }
