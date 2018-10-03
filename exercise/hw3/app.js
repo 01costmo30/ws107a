677914956dbdfd1f72ff081227a7eda068331146
01costmo30 <01lu301998@gmail.com>
2018-10-03 09:32:14 +0800

    update upstream file

--- exercise/hw3/app.js
+++ exercise/hw3/app.js
@@ -4,9 +4,9 @@ const app = module.exports = new Koa();
 app.use(async function(ctx) {
   // console.log('url=', ctx.url)
   switch (ctx.url) {
-    case '/hello': ctx.body = '你好'; break
-    // '/name' : ...; break
-    // '/id'   : ...; break
+    case '/hello': ctx.type = 'text/html;charset=utf-8'; ctx.body = '你好'; break;
+    case '/name' : ctx.type = 'text/html;charset=utf-8'; ctx.body = '陸佑函'; break;
+    case '/id'   : ctx.type = 'text/html;charset=utf-8'; ctx.body = '110510501'; break;
     default : ctx.status = 404
   }
   // ctx.body = 'Hello World';
